af5852b8933859b4f51302faa58097d281377b8e
OliverBenz <oliver.benz@outlook.com>
2020-09-13 14:55:30 +0200

    Fixed extra bracket, Refactoring

--- Light.py
+++ Light.py
@@ -8,27 +8,27 @@ class Light:
     self.portGreen = pGreen
     self.portBlue = pBlue
 
-  def setColor(self, r, g, b):
-    self.red = r
-    self.green = g
-    self.blue = b
+  def setColor(self, red, green, blue):
+    self.red = red
+    self.green = green
+    self.blue = blue
   
   def updateLights(self):
     pi.set_PWM_dutycycle(self.portRed, self.red)
-    pi.set_PWM_dutycycle(self.portGreen), self.green)
+    pi.set_PWM_dutycycle(self.portGreen, self.green)
     pi.set_PWM_dutycycle(self.portBlue, self.blue)
   
   def changeColor(self, status):
     if status == "Offline":
       self.setColor(0, 0, 0)       # Off
     
-    elif status == "Error" or status == "Cancelling":
+    elif status in ["Error", "Cancelling"]:
       self.setColor(255, 0, 0)     # Red
 
     elif status == "Printing":
       self.setColor(120, 120, 120) # White
 
-    elif status == "Pausing" or status == "Paused":
+    elif status in ["Pausing", "Paused"]:
       self.setColor(255, 255, 0)   # Yellow
 
     elif status == "Operational":
